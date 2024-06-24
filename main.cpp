93292be4a129e0eabee517352062a774c0b4097a
Aman Sharma <mail.aman@protonmail.com>
2024-06-24 22:19:33 +0100

    added 3d lib

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "VAO.h"
 #include "VBO.h"
